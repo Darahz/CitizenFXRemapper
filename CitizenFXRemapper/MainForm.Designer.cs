ca71a64950fd4b688cadbdb65105ae44a5729e35
andre <darahzmail@gmail.com>
2022-03-11 11:17:31 +0100

    Created new form to display bindings

---
+++ CitizenFXRemapper/MainForm.Designer.cs
@@ -0,0 +1,288 @@
+﻿
+namespace CitizenFXRemapper
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.TopPanel = new System.Windows.Forms.Panel();
+            this.FormHeader = new System.Windows.Forms.Panel();
+            this.MinimizeButton = new System.Windows.Forms.Button();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.FormTitle = new System.Windows.Forms.Label();
+            this.iconBox = new System.Windows.Forms.PictureBox();
+            this.BottomToolStrip = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.LoadedConfig = new System.Windows.Forms.ToolStripComboBox();
+            this.customTabControl1 = new CitizenFXRemapper.Controls.CustomTabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.PaddingCuzFuckYou = new System.Windows.Forms.Panel();
+            this.mainPanel.SuspendLayout();
+            this.TopPanel.SuspendLayout();
+            this.FormHeader.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconBox)).BeginInit();
+            this.BottomToolStrip.SuspendLayout();
+            this.customTabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Controls.Add(this.customTabControl1);
+            this.mainPanel.Controls.Add(this.TopPanel);
+            this.mainPanel.Controls.Add(this.BottomToolStrip);
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(0, 0);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(1427, 684);
+            this.mainPanel.TabIndex = 0;
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.Controls.Add(this.FormHeader);
+            this.TopPanel.Controls.Add(this.panel3);
+            this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopPanel.Location = new System.Drawing.Point(0, 0);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Size = new System.Drawing.Size(1427, 33);
+            this.TopPanel.TabIndex = 14;
+            // 
+            // FormHeader
+            // 
+            this.FormHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(62)))), ((int)(((byte)(67)))));
+            this.FormHeader.Controls.Add(this.MinimizeButton);
+            this.FormHeader.Controls.Add(this.CloseButton);
+            this.FormHeader.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.FormHeader.Location = new System.Drawing.Point(287, 0);
+            this.FormHeader.Name = "FormHeader";
+            this.FormHeader.Size = new System.Drawing.Size(1140, 33);
+            this.FormHeader.TabIndex = 13;
+            // 
+            // MinimizeButton
+            // 
+            this.MinimizeButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(76)))), ((int)(((byte)(81)))));
+            this.MinimizeButton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.MinimizeButton.FlatAppearance.BorderSize = 0;
+            this.MinimizeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.MinimizeButton.ForeColor = System.Drawing.Color.White;
+            this.MinimizeButton.Location = new System.Drawing.Point(1048, 0);
+            this.MinimizeButton.Name = "MinimizeButton";
+            this.MinimizeButton.Size = new System.Drawing.Size(46, 33);
+            this.MinimizeButton.TabIndex = 1;
+            this.MinimizeButton.Text = "-";
+            this.MinimizeButton.UseVisualStyleBackColor = false;
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(76)))), ((int)(((byte)(81)))));
+            this.CloseButton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.CloseButton.FlatAppearance.BorderSize = 0;
+            this.CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CloseButton.ForeColor = System.Drawing.Color.White;
+            this.CloseButton.Location = new System.Drawing.Point(1094, 0);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(46, 33);
+            this.CloseButton.TabIndex = 0;
+            this.CloseButton.Text = "x";
+            this.CloseButton.UseVisualStyleBackColor = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(62)))), ((int)(((byte)(67)))));
+            this.panel3.Controls.Add(this.FormTitle);
+            this.panel3.Controls.Add(this.iconBox);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(287, 33);
+            this.panel3.TabIndex = 15;
+            // 
+            // FormTitle
+            // 
+            this.FormTitle.AutoSize = true;
+            this.FormTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.FormTitle.ForeColor = System.Drawing.Color.White;
+            this.FormTitle.Location = new System.Drawing.Point(44, 9);
+            this.FormTitle.Name = "FormTitle";
+            this.FormTitle.Size = new System.Drawing.Size(153, 20);
+            this.FormTitle.TabIndex = 15;
+            this.FormTitle.Text = "CitizenFXRemapper";
+            // 
+            // iconBox
+            // 
+            this.iconBox.BackColor = System.Drawing.Color.Transparent;
+            this.iconBox.Cursor = System.Windows.Forms.Cursors.Help;
+            this.iconBox.Dock = System.Windows.Forms.DockStyle.Left;
+            this.iconBox.Image = ((System.Drawing.Image)(resources.GetObject("iconBox.Image")));
+            this.iconBox.Location = new System.Drawing.Point(0, 0);
+            this.iconBox.Name = "iconBox";
+            this.iconBox.Size = new System.Drawing.Size(45, 33);
+            this.iconBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.iconBox.TabIndex = 7;
+            this.iconBox.TabStop = false;
+            // 
+            // BottomToolStrip
+            // 
+            this.BottomToolStrip.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(49)))), ((int)(((byte)(54)))));
+            this.BottomToolStrip.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.BottomToolStrip.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.BottomToolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.LoadedConfig});
+            this.BottomToolStrip.Location = new System.Drawing.Point(0, 659);
+            this.BottomToolStrip.Name = "BottomToolStrip";
+            this.BottomToolStrip.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.BottomToolStrip.Size = new System.Drawing.Size(1427, 25);
+            this.BottomToolStrip.TabIndex = 18;
+            this.BottomToolStrip.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.ForeColor = System.Drawing.Color.White;
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(81, 22);
+            this.toolStripLabel1.Text = "Load Config : ";
+            // 
+            // LoadedConfig
+            // 
+            this.LoadedConfig.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.LoadedConfig.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(49)))), ((int)(((byte)(54)))));
+            this.LoadedConfig.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.LoadedConfig.ForeColor = System.Drawing.Color.White;
+            this.LoadedConfig.Name = "LoadedConfig";
+            this.LoadedConfig.Size = new System.Drawing.Size(1333, 25);
+            // 
+            // customTabControl1
+            // 
+            this.customTabControl1.Controls.Add(this.tabPage1);
+            this.customTabControl1.Controls.Add(this.tabPage2);
+            this.customTabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.customTabControl1.ItemSize = new System.Drawing.Size(150, 30);
+            this.customTabControl1.Location = new System.Drawing.Point(0, 33);
+            this.customTabControl1.Multiline = true;
+            this.customTabControl1.Name = "customTabControl1";
+            this.customTabControl1.SelectedIndex = 0;
+            this.customTabControl1.Size = new System.Drawing.Size(1427, 626);
+            this.customTabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.customTabControl1.TabIndex = 17;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(49)))), ((int)(((byte)(54)))));
+            this.tabPage1.Location = new System.Drawing.Point(0, 30);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1427, 596);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Keybinds";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(49)))), ((int)(((byte)(54)))));
+            this.tabPage2.Controls.Add(this.richTextBox1);
+            this.tabPage2.Controls.Add(this.PaddingCuzFuckYou);
+            this.tabPage2.Location = new System.Drawing.Point(0, 30);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1427, 596);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Raw Config";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(49)))), ((int)(((byte)(54)))));
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(30, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1394, 590);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // PaddingCuzFuckYou
+            // 
+            this.PaddingCuzFuckYou.Dock = System.Windows.Forms.DockStyle.Left;
+            this.PaddingCuzFuckYou.Location = new System.Drawing.Point(3, 3);
+            this.PaddingCuzFuckYou.Name = "PaddingCuzFuckYou";
+            this.PaddingCuzFuckYou.Size = new System.Drawing.Size(27, 590);
+            this.PaddingCuzFuckYou.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(49)))), ((int)(((byte)(54)))));
+            this.ClientSize = new System.Drawing.Size(1427, 684);
+            this.Controls.Add(this.mainPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainForm";
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
+            this.TopPanel.ResumeLayout(false);
+            this.FormHeader.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconBox)).EndInit();
+            this.BottomToolStrip.ResumeLayout(false);
+            this.BottomToolStrip.PerformLayout();
+            this.customTabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Panel TopPanel;
+        private System.Windows.Forms.Panel FormHeader;
+        private System.Windows.Forms.Button MinimizeButton;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox iconBox;
+        private System.Windows.Forms.Label FormTitle;
+        private Controls.CustomTabControl customTabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel PaddingCuzFuckYou;
+        private System.Windows.Forms.ToolStrip BottomToolStrip;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripComboBox LoadedConfig;
+    }
+}
---
+++ CitizenFXRemapper/MainForm.cs
@@ -0,0 +1,61 @@
+﻿using CitizenFXRemapper.Classes;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CitizenFXRemapper
+{
+    public partial class MainForm : Form
+    {
+
+        private readonly string CFGFileLocation = $"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\\CitizenFX\\fivem.cfg";
+        private readonly string MainBackupDirectory = ".\\CFGS\\";
+        private readonly string MainBackupLocaton = ".\\CFGS\\BackupConfig.cfg";
+        
+
+        public MainForm()
+        {
+            InitializeComponent();
+            CloseButton.Click += (s, e) => { Application.Exit(); };
+            MinimizeButton.Click += (s, e) => { this.WindowState = FormWindowState.Minimized; };
+            customTabControl1.SelectedIndexChanged += (s, e) =>
+            {
+                BottomToolStrip.Visible = customTabControl1.SelectedIndex == 0;
+                BottomToolStrip.BringToFront();
+            };
+            FormHeader.MouseDown += (s, e) =>
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    Borderless.ReleaseCapture();
+                    Borderless.SendMessage(Handle, Borderless.WM_NCLBUTTONDOWN, Borderless.HT_CAPTION, 0);
+                }
+            };
+            //MainForm Events
+            FormTitle.Text = $"{Application.ProductName} by {Application.CompanyName}";
+            this.Load += (s, e) => {
+                Directory.CreateDirectory(MainBackupDirectory);
+                if (!File.Exists(MainBackupLocaton)) File.Copy(CFGFileLocation, $"{MainBackupLocaton}");
+                Confighandler.ReadConfig(Filename: CFGFileLocation, RichTextBox:richTextBox1) ;
+                LoadedConfig.Items.Add(CFGFileLocation);
+                foreach (string File in Directory.GetFiles(MainBackupDirectory,"*.cfg"))
+                {
+                    LoadedConfig.Items.Add(File);
+                }
+            };
+
+            LoadedConfig.SelectedIndexChanged += (s, e) => {
+                Confighandler.ReadConfig(Filename: LoadedConfig.Text, RichTextBox: richTextBox1);
+            };
+            iconBox.MouseUp += (s, e) =>
+            {
+                if (e.Button != MouseButtons.Left) return;
+                if(MessageBox.Show($"This opens up the Github page for {Application.ProductName}. Do you want to continue?","Continue?",MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    Process.Start("https://github.com/Darahz/CitizenFXRemapper");
+                }
+            };
+        }
+    }
+}
